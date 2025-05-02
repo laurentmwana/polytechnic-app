c1e1f98847dad0ba0a8b56cd3b0e95207552b647
Laurent Mwanamputu <laurentmwn@gmail.com>
2025-05-02 04:39:20 +0100

    resolve: error parsing config

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { AUTH_ROUTES, GUEST_ROUTES } from './constants/granted'
 import { webRoute } from './lib/route'
 import { getToken } from 'next-auth/jwt'
 
+
 const secret = process.env.NEXTAUTH_SECRET
 
 export default async function middleware(req: NextRequest) {
@@ -21,8 +22,7 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next()
 }
 
+
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|.*\\.(png|jpg|jpeg|svg|gif|ico)$).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
