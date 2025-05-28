6c940b83e61fc493d692955f7edf341af131ccab
Labeya <laurentmwn@gmail.com>
2025-05-28 02:05:11 +0100

    init shadcn-vue

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="h-full">
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
-})
+  modules: ["@nuxt/image", "shadcn-nuxt", "@nuxtjs/color-mode"],
+  css: ["~/assets/css/app.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+});
